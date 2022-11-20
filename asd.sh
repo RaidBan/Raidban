c598dbf5ca1021e1ef9145ce33abfe396f43a2ae
RaidBan <nagib10000@mail.ru>
2022-11-20 20:17:18 +0700

    first commit

---
+++ asd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir /var/log/path
+
+ls -R ~ > /var/log/path/list
+
+gzip /var/log/path/list
+
+ls /dev | grep sd > /var/log/path/1
+
+echo 'Вставьте флешку и нажмине ENTER'
+
+if read
+then
+
+echo 'asd'
+fi
+ls /dev | grep sd > /var/log/path/2
+
+
+diff /var/log/path/1 /var/log/path/2 | grep sd | grep 1 > /var/log/path/diff
+cat /var/log/path/diff
+path=$( cut -c 3-6 /var/log/path/diff )
+
+#mkdir /mnt/1
+echo $path
+#mount /dev/$path /mnt/1
+
+#cp /var/log/path/list.gz /mnt/1
+
+rm -rf /var/log/path
+
+echo 'Готово'
+
+
