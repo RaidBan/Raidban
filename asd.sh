e334c0d2f521d90a8f5a3fc3c8c56f49804ad6ee
RaidBan <nagib10000@mail.ru>
2022-11-20 20:38:09 +0700

    2

--- asd.sh
+++ asd.sh
@@ -12,22 +12,17 @@ echo 'Вставьте флешку и нажмине ENTER'
 
 if read
 then
-
-echo 'asd'
-fi
 ls /dev | grep sd > /var/log/path/2
-
+fi
 
 diff /var/log/path/1 /var/log/path/2 | grep sd | grep 1 > /var/log/path/diff
 cat /var/log/path/diff
 path=$( cut -c 3-6 /var/log/path/diff )
 
-#mkdir /mnt/1
+mkdir /mnt/1
 echo $path
-#mount /dev/$path /mnt/1
-
-#cp /var/log/path/list.gz /mnt/1
-
+mount /dev/$path /mnt/1
+cp /var/log/path/list.gz /mnt/1
 rm -rf /var/log/path
 
 echo 'Готово'
